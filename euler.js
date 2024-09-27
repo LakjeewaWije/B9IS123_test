78a6b6c975dc92192ac70599d7b3f2b7febd0085
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-09-27 14:20:40 +0100

    Added : 2nd question

--- euler.js
+++ euler.js
@@ -15,19 +15,26 @@ alert("Loaded Euler JS");
 // answers
 
 // 1.
-a=3;
-b=5;
-n = 1000;
+
 total = 0;
-for (i = 0; i < n; i++) {
-    if (i % a == 0 || i % b == 0) total += i;
+for (i = 0; i < 1000; i++) {
+    if (i % 3 == 0 || i % 5 == 0) total += i;
 };
 
 alert('Find the sum of all the multiples of 3 or 5 below 1000 : ' + total);
 
 
 // 2.
-
-function findMultiples(a,b,n) {
-    
+a = 3;
+b = 5;
+n = 1000;
+function findMultiples(aval, bval, nval) {
+    total = 0;
+    for (i = 0; i < nval; i++) {
+        if (i % aval == 0 || i % bval == 0) total += i;
+    };
+    return total;
 }
+
+const totalFinal = findMultiples(a,b,n);
+alert('multiples of a or b below n : '+totalFinal);
