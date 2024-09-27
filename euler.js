c47602e02376af093469afbba2fb9caf189befe4
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-09-27 13:34:47 +0100

    Added : for 1000

--- euler.js
+++ euler.js
@@ -2,7 +2,8 @@ alert('Project euler loaded');
 // 1. https://projecteuler.net/problem=1
 
 total = 0;
-for (i = 0; i < 10; i++) {
+number = 1000;
+for (i = 0; i < number; i++) {
     if (i % 3 == 0 || i % 5 == 0) total += i;
 };
 alert(total);
