0a5d5af194aaa890056d9bf56e0df4eff11375a9
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-09-27 13:33:20 +0100

    Added :  project euler first q

--- euler.js
+++ euler.js
@@ -1,3 +1,8 @@
-// https://projecteuler.net/problem=1
-
 alert('Project euler loaded');
+// 1. https://projecteuler.net/problem=1
+
+total = 0;
+for (i = 0; i < 10; i++) {
+    if (i % 3 == 0 || i % 5 == 0) total += i;
+};
+alert(total);
