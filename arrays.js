82d03a45672a54b5fe267f50c4c95a1a198e54f4
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-04 14:37:02 +0100

    Added : array 1

--- arrays.js
+++ arrays.js
@@ -17,4 +17,21 @@ l is a list of integers
 find the sum of all the multiples of elements of a in l
 */
 
-alert("Array Js loaded")
+alert("Array Js loaded");
+
+a = 3;
+b = 5;
+l = [1,3,6,7,8]
+function sumOfMultiples(valA, valB, list) {
+    sum = 0;
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] % valA == 0 || list[index] % valB == 0) {
+            sum += list[index];
+        }
+
+    }
+
+    alert(`sum of ${l.toString()} : `,sum);
+}
+
+sumOfMultiples();
