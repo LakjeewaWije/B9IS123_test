9b5ab2b3a7e7b370b309a12f4f3843460ecf7004
Lakjeewa Wijebandara <lakjeewa.wijebandara@gmail.com>
2024-10-04 14:15:01 +0100

    Added :  array js

---
+++ arrays.js
@@ -0,0 +1,20 @@
+/*Arrays:
+a.
+Create a function which takes parameters a,b,l
+l is a list of integers
+find the sum of all the multiples of a or b in l
+
+b.
+Create a function which takes parameters a,l
+a is a list of two integers
+l is a list of integers
+find the sum of all the multiples of elements of a in l
+
+c.
+Create a function which takes parameters a,l
+a is a list of integers
+l is a list of integers
+find the sum of all the multiples of elements of a in l
+*/
+
+alert("Array Js loaded")
